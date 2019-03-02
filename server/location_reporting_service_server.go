1e42ae0ed983f9ec3d7c5a0d412c2b38b4474972
Vladimir Voinea <voineavladimir@gmail.com>
2019-03-02 17:43:48 +0100

    Accept command line certificate and key

--- server/location_reporting_service_server.go
+++ server/location_reporting_service_server.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net"
+	"os"
 
 	pb "github.com/vladimir-voinea/location-report-app/server/location_reporting_service"
 
@@ -20,23 +23,44 @@ type server struct{}
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) PushLocations(ctx context.Context, in *pb.PushLocationsRequest) (*pb.PushLocationsResponse, error) {
-	log.Printf("Received from: %s", in.GetLocations()[0].Latitude, in.GetLocations()[0].Longitude, in.GetLocations()[0].Bearing)
+	log.Printf("Received push request")
+	for index, location := range in.GetLocations() {
+		fmt.Printf("%d. Lat: %f Lon: %f Bearing: %f\n", index, location.Latitude, location.Longitude, location.Bearing)
+	}
 	response := &pb.PushLocationsResponse{}
 	response.Success = true
 	return response, nil
 }
 
+func GetCredentials() (cert string, key string, err error) {
+	var cert_file string
+	var key_file string
+
+	flag.StringVar(&cert_file, "cert_file", "", "Certificate file in PEM format")
+	flag.StringVar(&key_file, "key_file", "", "Key file")
+
+	flag.Parse()
+
+	fmt.Println("Cert file: " + cert_file)
+	fmt.Println("Key file: " + key_file)
+
+	return cert_file, key_file, nil
+}
+
 func main() {
+	cert_file, key_file, err := GetCredentials()
+	if err != nil {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	var opts []grpc.ServerOption
-
-	certFile := "server1.pem"
-	keyFile := "server1.key"
-
-	creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+	creds, err := credentials.NewServerTLSFromFile(cert_file, key_file)
 	if err != nil {
 		log.Fatalf("Failed to generate credentials %v", err)
 	}
